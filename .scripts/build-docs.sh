25de29882fed71b896b6cc3338f6e2ed384bc084
chernenko <chernenko@protei-lab.ru>
2026-04-08 17:09:59 +0300

    Added script to create release md docs

--- .scripts/build-docs.sh
+++ .scripts/build-docs.sh
@@ -1,1 +1,18 @@
-find 'C++ Q@A' -name "*.md" ! -name "*MOC*" -print0 | sort -z | while IFS= read -r -d '' file; do     filename=$(basename "$file" .md)    ;     echo "## $filename" >> full_export.md;     echo "" >> full_export.md;     cat "$file" >> full_export.md;     echo "" >> full_export.md; echo "" >> full_export.md; done
+#!/bin/bash
+
+output_file="${1:-full_export.md}"
+
+[ -f "$output_file" ] && echo "Файл $output_file существует и будет перезаписан"
+
+> "$output_file"
+
+find . -name "*.md" ! -name "$output_file" ! -name "*MOC*" -print0 | sort -z | while IFS= read -r -d '' file; do
+    filename=$(basename "$file" .md)
+    echo "## $filename" >> "$output_file"
+    echo "" >> "$output_file"
+    cat "$file" >> "$output_file"
+    echo "" >> "$output_file"
+    echo "" >> "$output_file"
+done
+
+echo "Готово! Файл сохранен как: $output_file"
